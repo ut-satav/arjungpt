cffb599d099d88e81d227a2a8b1d1368c9984974
KiloSat <dipeshssatav@gmail.com>
2024-04-16 14:27:31 +0900

    changes

--- pages/4_Chat_Model.py
+++ pages/4_Chat_Model.py
@@ -4,41 +4,67 @@ st.set_page_config(page_title="Chat Model")
 st.markdown("# Chat Model")
 st.sidebar.header("Chat Model")
 
-def get_chat_model(question):
-    import torch
-    from transformers import pipeline
+def get_chat_model(question,uploaded_file):
+    from langchain_community.document_loaders import TextLoader
+    from langchain_mistralai.chat_models import ChatMistralAI
+    from langchain_mistralai.embeddings import MistralAIEmbeddings
+    from langchain_community.vectorstores import FAISS
+    from langchain.text_splitter import RecursiveCharacterTextSplitter
+    from langchain.chains.combine_documents import create_stuff_documents_chain
+    from langchain_core.prompts import ChatPromptTemplate
+    from langchain.chains import create_retrieval_chain
+    import os
+    api_key=os.environ['api_key'] 
+    # Load data
+    loader = TextLoader(uploaded_file)
+    docs = loader.load()
+    # Split text into chunks
+    text_splitter = RecursiveCharacterTextSplitter()
+    documents = text_splitter.split_documents(docs)
+    # Define the embedding model
+    embeddings = MistralAIEmbeddings(model="mistral-embed", mistral_api_key=api_key)
+    # Create the vector store
+    vector = FAISS.from_documents(documents, embeddings)
+    # Define a retriever interface
+    retriever = vector.as_retriever()
+    # Define LLM
+    model = ChatMistralAI(mistral_api_key=api_key)
+    # Define prompt template
+    prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
 
-    generate_text = pipeline(
-        model="Ketak-ZoomRx/Drug_Prompt_Ollama_67k",
-        torch_dtype="auto",
-        trust_remote_code=True,
-        use_fast=True,
-        device_map={"": "cuda:0"},
-    )
+    <context>
+    {context}
+    </context>
 
-    res = generate_text(
-    question,
-    min_new_tokens=2,
-    max_new_tokens=256,
-    do_sample=False,
-    num_beams=1,
-    temperature=float(0.0),
-    repetition_penalty=float(1.2),
-    renormalize_logits=True)
+    Question: {input}""")
 
-    return res[0]["generated_text"]
+    # Create a retrieval chain to answer questions
+    document_chain = create_stuff_documents_chain(model, prompt)
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+    response = retrieval_chain.invoke({"input": question})
+    
+    return response["answer"]
 
 
 def main():
+    context = st.text_input(label='Context Article', 
+                               placeholder='Paste your article here',key='context')
+    
+    from io import StringIO
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is None:
+        st.info("Please choose a txt file ..")
+    
     question =  st.text_input(label='Question', 
                                placeholder='Type your question here',key='question')+'?'
     # Wait until the question is not empty
     if not question:
         st.info("Please enter a question...")
 
-    if question :
+    answer = 'None'    
+    if question and uploaded_file:
         # Run the selected function based on the user's choice
-        answer = get_chat_model(question)
+        answer = get_chat_model(question,uploaded_file)
         # Once text input is provided, display it
         st.write(answer)
 
